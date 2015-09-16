8784e136cf1f53bb96fba8a12793ad6fd503127d
pznp111 <a0108355@nus.edu.sg>
2015-09-16 20:45:06 +0800

    added TDD

--- src/TextBuddyCE2TDD.java
+++ src/TextBuddyCE2TDD.java
@@ -2,13 +2,20 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextBuddyCE2TDD {
 	
+	@Before
+	public void setUp(){
+		TextBuddy.inputfile= "mytextfile.txt";
+		
+	}
+	
 	@Test
     public void testEmptySearch() throws IOException{
-		assertEquals("You have not add task, nothing found",TextBuddy.executeCommand("search hw"));
+		assertEquals("You have not add task, nothing found",TextBuddy.executeSearch("hw"));
 	}
 	
 
