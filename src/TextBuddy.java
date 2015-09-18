7389cbb44a129b317c3554c365998f05b1ccc21c
pznp111 <a0108355@nus.edu.sg>
2015-09-18 21:08:20 +0800

    improve format of TextBuddy.java and add in comments

--- src/TextBuddy.java
+++ src/TextBuddy.java
@@ -3,14 +3,17 @@
  * A0108355H
  * 
  * file will be saved to disk after each operation
+ * tasklist.txt is the file in which tasks are saved to
  * 
  * program will catch a few errors including:
  * 1. invalid commands
  * 2. file name not specified
  * 3. wrong format of commands such as not a number after the "delete" commands
  * 4. trying to delete a item that has not been created yet
+ * 5. display when there is no item added
+ * 6. try to sort when there is no item to sort
  * 
- * here is the default input:
+ * here is the default input for CE1:
    add little brown fox
    display
    add jumped over the moon
@@ -20,12 +23,23 @@
    clear
    display
    exit
+   
+   TDD for CE2:
+   1. testEmptySearch()
+   2. testEmptySort()
+   3. testDisplay()  //test empty display
+   4. testAdd1() and testAdd2()
+   5. testDisplay() //test display when there are items
+   6. testSort()
+   7. testSearch() //nothing found
+   6. testDelete()
+   7. testSearch() // find a task
+   
+   comment testEmptySearch() and testEmptySort() when items added, so as to pass unit test cases
+
  */
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -36,11 +50,13 @@ import java.util.Scanner;
 
 public class TextBuddy {
 	
+	//variables
 	static String inputfile;
 	private static ArrayList<String> list = new ArrayList<String>();
 	private static String file = "tasklist.txt";
 	private static Scanner sc = new Scanner(System.in);
 	
+	//commands
 	private static final String ADD_CMD = "add";
 	private static final String DELETE_CMD = "delete";
 	private static final String DISPLAY_CMD = "display";
@@ -49,6 +65,7 @@ public class TextBuddy {
 	private static final String SEARCH_CMD = "search";
 	private static final String SORT_CMD = "sort";
 	
+	//messages
 	private static final String ERROR_FILE_NAME_NOT_SPECIFIED = "Error, file name not specified";
 	private static final String ADD_MESSAGE = "added to %s: \"%s\"";
 	private static final String DELETE_MESSAGE = "deleted from %s: \"%s\"";
@@ -79,8 +96,9 @@ public class TextBuddy {
 			String userCommand = sc.nextLine();
 			showToUser(userCommand);
 			String feedback = executeCommand(userCommand);
-			showToUser(feedback);
 			saveFile();
+			showToUser(feedback);
+		
 		}	
 	}
 	
@@ -91,33 +109,33 @@ public class TextBuddy {
 	/*
 	 * this function execute the commands   
 	 */
-	static String executeCommand(String userCommand) throws IOException {		
-		String command=getCommand(userCommand);
-		String info=getInfo(userCommand);
+	public static String executeCommand(String userCommand) throws IOException {		
+		String command = getCommand(userCommand);
+		String info = getInfo(userCommand);
 		
-		if (command.equals(ADD_CMD)){			
+		if (command.equals(ADD_CMD)) {			
 			return executeAdd(info);			
-		} else if (command.equals(DISPLAY_CMD)){			
+		} else if (command.equals(DISPLAY_CMD)) {			
 			 executeDisplay(info);		
-		} else if (command.equals(DELETE_CMD)){		
+		} else if (command.equals(DELETE_CMD)) {		
 			return executeDelete(info);			
-		} else if (command.equals(CLEAR_CMD)){			
+		} else if (command.equals(CLEAR_CMD)) {			
 			return executeClear();			
-		} else if (command.equals(EXIT_CMD)){			
+		} else if (command.equals(EXIT_CMD)) {			
 			executeExit();		
-		} else if (command.equals(SEARCH_CMD)){
+		} else if (command.equals(SEARCH_CMD)) {
 			executeSearch(info);
-		} else if (command.equals(SORT_CMD)){
+		} else if (command.equals(SORT_CMD)) {
 			executeSort();
-		} else{			
+		} else {			
 			return (FORMAT_ERROR);			
 		}		
 		return "";
 	}
 
-	static String executeSort() {		
+	public static String executeSort() {		
 	String sortMSG = "";	
-	if (list.size() == 0){
+	if (list.size() == 0) {
 	return EMPTY_SORT_MESSAGE;	
 	}
 	
@@ -126,31 +144,31 @@ public class TextBuddy {
 	return SUCCEEFUL_SORT_MESSAGE;
 	}
 
-	static String executeSearch(String info) {	
+	public static String executeSearch(String info) {	
 		boolean isFound = false;
 		String lineFound = "";
 		
-		if (list.size() == 0){
+		if (list.size() == 0) {
 			return EMPTY_ERROR_MESSAGE;
 		}
 		
-		for (int i =0; i < list.size(); i++){
-			if (list.get(i).contains(info)){
+		for (int i =0; i < list.size(); i++) {
+			if (list.get(i).contains(info)) {
 				isFound = true;
 				lineFound ="found, the task is " + list.get(i);
 			}
 		}
 		
-		if (isFound == true){
+		if (isFound == true) {
 			return lineFound;
 		} else {
 		return NOTFOUND_ERROR_MESSAGE;
 		}
 	}
 
-	private static void executeExit() throws IOException  {			
+	public static void executeExit() throws IOException {			
 		    PrintWriter writer = new PrintWriter(inputfile, "UTF-8");
-		    for (int i=0; i<list.size(); i++ ){	   
+		    for (int i=0; i<list.size(); i++ ) {	   
 		    	writer.write(list.get(i));
 		    	writer.write(System.getProperty("line.separator"));		    	
 		    }
@@ -159,17 +177,17 @@ public class TextBuddy {
 		    System.exit(0);
 	}
 
-	static String executeClear() {		
+	public static String executeClear() {		
 		list.clear();
 		return String.format(CLEAR_MESSAGE, inputfile);		
 	}
 
-	static String executeDelete(String info)  {		
+	public static String executeDelete(String info) {		
 		String returnInfo="";		
-		if (isNumeric(info)){			
+		if (isNumeric(info)) {			
 			int num=Integer.parseInt(info);
 			
-			if (num > list.size()){				
+			if (num > list.size()) {				
 			returnInfo=DELETE_ERROR_MESSAGE;				
 			} else {			
 			list.remove(num-1);
@@ -183,15 +201,20 @@ public class TextBuddy {
 		return returnInfo;		
 	}
 
-	static String executeAdd(String info) {		
+	public static String executeAdd(String info) {		
 		list.add(info);
 		String returnInfo=String.format(ADD_MESSAGE, inputfile,info);
 		return returnInfo;		
 	}
-
-	static boolean executeDisplay(String info) {
+	
+	/**
+	 * this function displays tasks that added
+	 * @return true if there are tasks added, else return false
+	 *.@return EMPTY_MESSAGE if there is no task
+	 */
+	public static boolean executeDisplay(String info) {
 		boolean valid = false;
-		if (list.size()==0){			
+		if (list.size()==0) {			
 			showToUser(String.format(EMPTY_MESSAGE, inputfile));			
 		} else {	
 		for(int i=0; i < list.size(); i++){			
@@ -217,8 +240,8 @@ public class TextBuddy {
 	 * @param userCommand is input
 	 */
 	private static String getInfo(String userCommand) {
-		int i=userCommand.indexOf(" ");
-		String info=userCommand.substring(i+1,userCommand.length());
+		int i = userCommand.indexOf(" ");
+		String info = userCommand.substring(i+1,userCommand.length());
 		return info;		
 	}
 
@@ -246,7 +269,7 @@ public class TextBuddy {
 	 * @return message of [filename] cleared.
 	 * @throws IOException if file does not exist.
 	 */
-	private static String emptyFile() throws IOException{		
+	private static String emptyFile() throws IOException {		
 		String content = "";
 		String result = file+" cleared";
 		File fileObj = new File(file);
@@ -261,16 +284,15 @@ public class TextBuddy {
 	 * this function save arraylist to a file.
 	 * @throws IOException  If file does not exist.
 	 */
-    private static void saveFile() throws IOException{   	
+    private static void saveFile() throws IOException {   	
     	File fileObj = new File(file);
     	String content = "";
     	BufferedWriter bw = new BufferedWriter(new FileWriter(file));
     	
-    	for (int i = 0; i < list.size(); i ++){
+    	for (int i = 0; i < list.size(); i ++) {
     		String lineToWrite = list.get(i);
     		content += lineToWrite + "\r\n";
     	}
-    	System.out.println(content);
     	bw.write(content);
     	bw.close();   	
     }	
